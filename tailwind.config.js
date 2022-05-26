a3524a50f1d64b0df3442c6acafb00ac134440e5
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-26 11:51:52 +0200

    style: added tailwind support for colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#DD5F63",
+        bg: "#191F2F",
+      },
+      fontFamily: {
+        nunito: ['"Nunito"'],
+        rubik: ['"Rubik"'],
+      },
+    },
   },
   plugins: [],
 };
