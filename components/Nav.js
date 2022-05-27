7887902be8e89c4a1f8299a034d20148fcaabad2
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-27 12:00:49 +0200

    feat: nav component added

---
+++ components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div>
+        <div>ICON</div>
+        <h1>TOMATODO</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Nav from "../components/Nav";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className=" bg-bg min-h-screen">
+      <div className="bg-white max-w-2xl min-h-screen mx-auto">
+        <Nav />
+      </div>
+    </div>
+  );
 }
