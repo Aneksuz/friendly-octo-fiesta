87b246026ee300834d8e6356bee2202fd4acc42b
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-28 12:33:24 +0200

    feat: added timer to index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import Nav from "../components/Nav";
+import Timer from "../components/Timer";
 
 export default function Home() {
   return (
     <div className=" bg-bg min-h-screen">
       <div className="bg-white max-w-2xl min-h-screen mx-auto">
         <Nav />
+        <Timer />
       </div>
     </div>
   );
