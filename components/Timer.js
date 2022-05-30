d46f5966f8c0a13586a03bae4a3641fa4bd65e6b
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-30 11:59:30 +0200

    style: added select none to timer

--- components/Timer.js
+++ components/Timer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Timer = () => {
-  return <div>Timer</div>;
+  return (
+    <div>
+      <h1 className="select-none ">25:00</h1>
+    </div>
+  );
 };
 
 export default Timer;
