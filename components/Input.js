88e6c97fb390d4b3811875663e93befdd486b918
Tobias Bergström <tobiasbergstrom@outlook.com>
2022-05-31 18:46:09 +0200

    style: fixed input

--- components/Input.js
+++ components/Input.js
@@ -4,9 +4,10 @@ const Input = ({ title, placeholder }) => {
   return (
     <div>
       <h2>{title}</h2>
-      <input className="bg-prim shadow-inner shadow-sm w-auto pr-2">
-        {placeholder}
-      </input>
+      <input
+        className="bg-prim shadow-inner w-auto pr-2 font-rubik text-base "
+        placeholder={placeholder}
+      ></input>
     </div>
   );
 };
